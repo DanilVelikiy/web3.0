ef559c30cd5b822cb5a38efe01ea82da54b975f1
danil velikiy <danil.velikiy@gmail.com>
2023-11-21 09:51:26 +0300

    работа над /projects

--- django_server/settings.py
+++ django_server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'django_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["django_server/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
